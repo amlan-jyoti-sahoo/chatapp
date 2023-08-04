42b4d12e10541d91885516bf4437121df670ebbe
Amlan2002 <iamamlan2002@gmail.com>
2023-08-04 12:20:51 +0530

    profile image added

--- screens/ProfileImage.js
+++ screens/ProfileImage.js
@@ -1,15 +1,22 @@
 import {Image, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 
 import {useDispatch, useSelector} from 'react-redux';
-import {userSlice} from '.././store/userSlice';
 
-function ProfileImage() {
+function ProfileImage({navigation}) {
   const user = useSelector(state => state.user.selectedUser);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: user.senderName,
+    });
+  }, []);
+
   console.log('ProfileImage', user);
+  const profileImage = user.profileImage;
   return (
     <View style={styles.container}>
-      <Image source={user.ProfileImage} style={styles.ProfileImage} />
+      <Image source={profileImage} style={styles.ProfileImage} />
     </View>
     // <Text></Text>
   );
@@ -20,9 +27,12 @@ export default ProfileImage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   ProfileImage: {
-    width: 300,
-    height: 300,
+    width: '100%',
+    maxHeight: 400,
+    backgroundColor: '#f90000',
   },
 });
