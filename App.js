dc96e5bdf93eb58b29a1b88b87b9db082407a4f4
Amlan2002 <iamamlan2002@gmail.com>
2023-08-17 16:37:13 +0530

    redux-persist implemented

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Provider} from 'react-redux';
 import {store} from './store';
+import {PersistGate} from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
 
 import AllChatScreen from './screens/AllChatScreen';
 import ChatScreen from './screens/ChatScreen';
@@ -12,34 +14,37 @@ import ProfileImage from './screens/ProfileImage';
 import PhotoRender from './screens/PhotoRender';
 import ImageFullScreen from './screens/ImageFullScreen';
 
+const persistor = persistStore(store);
 function App() {
   const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="AllChatScreen"
-            component={AllChatScreen}
-            options={{title: 'Chats'}}
-          />
-          <Stack.Screen
-            name="ChatScreen"
-            component={ChatScreen}
-            options={{
-              headerRight: () => (
-                <View style={styles.headerRightContainer}>
-                  <Icon name="videocam" size={24} color="#000000" />
-                  <Icon name="call" size={24} color="#000000" />
-                </View>
-              ),
-            }}
-          />
-          <Stack.Screen name="ProfileImage" component={ProfileImage} />
-          <Stack.Screen name="PhotoRender" component={PhotoRender} />
-          <Stack.Screen name="ImageFullScreen" component={ImageFullScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="AllChatScreen"
+              component={AllChatScreen}
+              options={{title: 'Chats'}}
+            />
+            <Stack.Screen
+              name="ChatScreen"
+              component={ChatScreen}
+              options={{
+                headerRight: () => (
+                  <View style={styles.headerRightContainer}>
+                    <Icon name="videocam" size={24} color="#000000" />
+                    <Icon name="call" size={24} color="#000000" />
+                  </View>
+                ),
+              }}
+            />
+            <Stack.Screen name="ProfileImage" component={ProfileImage} />
+            <Stack.Screen name="PhotoRender" component={PhotoRender} />
+            <Stack.Screen name="ImageFullScreen" component={ImageFullScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.js
+++ store/index.js
@@ -1,8 +1,36 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {userSlice} from './userSlice';
 
+import {combineReducers} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+let rootReducer = combineReducers({
+  user: userSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
